a0b2ed2de4887b301b603ca2da7d3525ab4a4b07
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2022-05-19 16:45:53 -0500

    implemented same changes as 1.16.x

--- src/main/java/com/floweytf/mobopacity/mixin/RenderTypeMixin.java
+++ src/main/java/com/floweytf/mobopacity/mixin/RenderTypeMixin.java
@@ -3,16 +3,15 @@ package com.floweytf.mobopacity.mixin;
 import com.floweytf.mobopacity.AlphaStatus;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import static net.minecraft.client.renderer.RenderStateShard.*;
 
 @Mixin(RenderType.class)
 public class RenderTypeMixin extends RenderStateShard {
@@ -35,6 +34,7 @@ public class RenderTypeMixin extends RenderStateShard {
     private static void handleArmorCutoutNoCull(ResourceLocation resourceLocation, CallbackInfoReturnable<RenderType> cir) {
         if(AlphaStatus.enabled()) {
             RenderType.CompositeState compositeState = RenderType.CompositeState.builder()
+                .setShaderState(RENDERTYPE_ARMOR_CUTOUT_NO_CULL_SHADER)
                 .setTextureState(new RenderStateShard.TextureStateShard(resourceLocation, false, false))
                 .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
                 .setWriteMaskState(COLOR_DEPTH_WRITE)
@@ -43,7 +43,7 @@ public class RenderTypeMixin extends RenderStateShard {
                 .setOverlayState(OVERLAY)
                 .setLayeringState(VIEW_OFFSET_Z_LAYERING)
                 .createCompositeState(true);
-            cir.setReturnValue(create("armor_transparent", DefaultVertexFormat.NEW_ENTITY, 7, 256, true, false, compositeState));
+            cir.setReturnValue(create("armor_transparent", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, true, false, compositeState));
         }
     }
 }
